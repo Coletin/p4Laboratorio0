a57d1629962c8c16194b151d425beb9707a43387
FedericoGutierrezS <federicogs99@gmail.com>
2022-03-16 21:23:28 -0300

    Implementación clases

--- clases.h
+++ clases.h
@@ -1,10 +1,42 @@
+#include <iostream>
+using namespace std;
 
 class Habitacion{
-   public:
-    Habitacion();
+    private:
+     int numero;
+     float precio;
+     int capacidad;
+    public:
+     Habitacion();
 };
 
 class Huesped{
+    private:
+     string nombre;
+     string email;
+     bool esFinger;
     public:
      Huesped();
+};
+
+class Reserva{
+    private:
+     int codigo;
+     DTFecha checkIn;
+     DTFecha checkOut;
+     EstadoReserva estado;
+    public:
+     virtual float calcularCosto() = 0;
+};
+
+class ReservaGrupal : public Reserva{
+    public:
+     virtual float calcularCosto();
+};
+
+class ReservaIndividual : public Reserva{
+    private:
+     bool pagado;
+    public:
+     virtual float calcularCosto();
 };
